4f5391aff9ad9c16629eb18a4511490ca4ac8dac
OhByeongHeon <hi1004@github.com>
2022-05-22 21:38:12 +0900

    modify main.js

--- js/main.js
+++ js/main.js
@@ -15,9 +15,11 @@ let tryArray = [];
 let missings = [];
 let checkInterval;
 let level = 0.5;
-let URL = `https://random-word.ryanrk.com/api/jp/word/random/${
-  level * 100 // ランダム単語50個を持ってくる
-}`;
+let URL;
+let time = 10;
+let seconds = 600;
+let min = '';
+let sec = '';
 
 /* モーダルを出力 */
 function init() {
@@ -32,6 +34,42 @@ function init() {
 function visibleModal() {
   const displayModal = document.querySelector('.start');
   displayModal.click();
+}
+
+// const setTime = setInterval(() => {
+//   time--;
+// }, 1000);
+// if (time < 0) {
+//   clearInterval(setTime);
+//   alert('시간초과');
+// }
+
+const timerLabel = document.querySelector('.time');
+const timeLimit = 10 * 1000;
+let startTime = Date.now();
+function updateTimer() {
+  const timeLeft = startTime + timeLimit - Date.now();
+  const timeoutId = setTimeout(() => {
+    updateTimer();
+  }, 10);
+  if (timeLeft < 0) {
+    clearTimeout(timeoutId);
+    return (timerLabel.textContent = '0.00');
+  }
+  return (timeLeft / 1000).toFixed(2);
+}
+
+/* 言語設定 */
+function language() {
+  if (JP.checked) {
+    URL = `https://random-word.ryanrk.com/api/jp/word/random/${
+      level * 200 // ランダム単語100個の中で６桁以下
+    }`;
+  } else if (EN.checked) {
+    URL = `https://random-word-api.herokuapp.com/word?number=${
+      level * 300 // ランダム単語150個の中で10桁以下
+    }`;
+  }
 }
 
 /* モーダルを隠す（Enter, ESCで）*/
@@ -50,17 +88,16 @@ const wordInput = document.querySelector('.word-input');
 let isPlaying = false;
 function run() {
   getWords(); // プログラムが始まると、getWords関数が実行し、単語をサーバーから持ってくる。
-
   isPlaying = true; // ゲームを始める
   wordInput.removeAttribute('disabled'); // .word-inputのdiabled属性を削除
   wordInput.placeholder = '';
   checkInterval = setInterval(checkStatus, 50);
-
   animate(); // animate関数を実行
 }
 
+/* ゲームを終了する */
 function checkStatus() {
-  if (missings.length === 25) {
+  if (missings.length === 30 || time === 0) {
     wordInput.setAttribute('disabled', '');
     isPlaying = false;
     clearInterval(checkInterval);
@@ -68,9 +105,6 @@ function checkStatus() {
 }
 
 /* axiosライブラリーを使い、単語をOpen APIで持ってくる */
-
-let sliceWords = [];
-
 let words = [];
 async function getWords() {
   await axios
@@ -96,40 +130,12 @@ async function getWords() {
     });
 }
 
-/* 単語を作るcreateWord()関数はそれぞれのclassを生成 */
-let oldWords = [];
-let wordArray = [];
-
-const createWords = setInterval(() => {
-  createWord();
-}, 6000); // 6秒ずつcreateWord()実行。
-
-function createWord() {
-  words = words.slice(0, 50);
-  for (let i = 0; i < 5; i++) {
-    const distance = Math.random() * i + 150;
-    let x = (canvas.width / 5) * i + distance;
-    let y = 0; // 初期y座標
-    let dy = Math.random() * 0.1 + (Math.random() * level + speed); // 初期y軸の速度
-
-    wordArray.push(new Word(x, y, dy, distance, words[0])); // class Wordを生成し、wordArrayに入れて配列に作る。
-    oldWords.push(words.shift()); // サーバーからもらった単語のwords配列の要素を最初から次々と入れる。
-  }
-
-  if (wordArray.length === 50) {
-    clearInterval(createWords); // 単語５０個が全部出たら、終わる。
-  }
-}
-
 const resetBtn = document.querySelector('.reset-btn');
 function animate() {
   /* requestAnimationFrameが１秒で６０回実行する*/
   const currentWordCount = document.querySelector('.word-count');
-  /* .count teg*/
   const currectCount = document.querySelector('.count');
-  /* .score teg */
   const currectScore = document.querySelector('.score');
-
   const tryCount = document.querySelector('.try-count');
   const life = document.querySelector('.life');
 
@@ -144,19 +150,47 @@ function animate() {
     ctx.fillStyle = colors[0];
     ctx.fillText('GAME OVER', innerWidth / 2, innerHeight / 2 - 60);
     resetBtn.classList.remove('invisible');
+    wordInput.value = '';
     return;
   }
+
   requestAnimationFrame(animate);
+
   for (let i = 0; i < wordArray.length; i++) {
     wordArray[i].update(); // それぞれのclass Wordにあるメソッドupdate()を実行。
   }
 
-  life.innerHTML = `Life : ${25 - missings.length}`;
+  life.innerHTML = `Life : ${30 - missings.length}`;
   tryCount.innerHTML = `Try : ${tryArray.length}`;
   currectCount.innerHTML = `Level : ${count}`; // count出力
   currectScore.innerHTML = `Score : ${score}`; // score出力
   currentWordCount.innerHTML = `Word : ${words.length}`; // Word出力
+  timerLabel.innerHTML = `Time [${updateTimer()}]`;
 }
+
+/* 単語を作るcreateWord()関数はそれぞれのclassを生成 */
+let oldWords = [];
+let wordArray = [];
+function createWord() {
+  words = words.slice(0, 60);
+  for (let i = 0; i < 3; i++) {
+    const distance = Math.random() * i + 150;
+    let x = (canvas.width / 3) * i + distance;
+    let y = 0; // 初期y座標
+    let dy = Math.random() * 0.1 + (Math.random() * level + speed); // 初期y軸の速度
+
+    wordArray.push(new Word(x, y, dy, distance, words[0])); // class Wordを生成し、wordArrayに入れて配列に作る。
+    oldWords.push(words.shift()); // サーバーからもらった単語のwords配列の要素を最初から次々と入れる。
+  }
+
+  if (wordArray.length === 60) {
+    clearInterval(createWords); // 単語６０個が全部出たら、終わる。
+  }
+}
+
+const createWords = setInterval(() => {
+  createWord();
+}, 6000); // 6秒ずつcreateWord()実行。
 
 /* classでWordを作る */
 const colors = ['#ff006e', '#14213d', '#8338ec', '#606c38']; // カラーセット
@@ -211,7 +245,6 @@ class Word {
         return missings.indexOf(element) === index;
       });
     }
-
     this.y += this.dy;
     this.draw();
     wordInput.focus();
@@ -223,14 +256,13 @@ function checkMatch(e) {
     tryArray.push(wordInput.value);
     if (missings.includes(wordInput.value)) {
       alert('이미 지나간 단어입니다.');
-      gameOver();
     } else if (answers.includes(wordInput.value)) {
       alert('이미 정답으로 입력했습니다.');
       wordInput.value = '';
     } else if (oldWords.includes(wordInput.value)) {
+      time = 10;
       count++;
       answers.push(wordInput.value);
-
       let inputValue = wordArray[oldWords.indexOf(wordInput.value)].word;
       if (inputValue.length > 10) {
         score += 100;
@@ -243,6 +275,7 @@ function checkMatch(e) {
       }
       wordArray[oldWords.indexOf(wordInput.value)].word = '';
       speed += 0.05;
+
       wordInput.classList.remove('is-invalid');
       wordInput.classList.add('is-valid');
       wordInput.value = '';
@@ -263,18 +296,6 @@ function enterEvent(e) {
 function resize() {
   canvas.width = window.innerWidth; // ブラウザの幅
   canvas.height = window.innerHeight; // ブラウザの高さ
-}
-
-function language() {
-  if (JP.checked) {
-    URL = `https://random-word.ryanrk.com/api/jp/word/random/${
-      level * 200 // ランダム単語50個を持ってくる
-    }`;
-  } else if (EN.checked) {
-    URL = `https://random-word-api.herokuapp.com/word?number=${
-      level * 200 // ランダム単語50個を持ってくる
-    }`;
-  }
 }
 
 init();
